b3821a0eb78edf738d6d834f2c467ae638603e4d
NatLop <nplopez_s@hotmail.com>
2023-03-01 16:13:53 -0600

    actualizacion

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,22 +3,22 @@ const { default: expect } = require('expect');
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    //
+    expect(calculator.sum(4,5)).toBe(9);
 })
 
 test('substracts a - b', () => {
-   //
+    expect(calculator.substract(4,5)).toBe(-1);
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(calculator.multiply(4,5)).toBe(20);
 })
 
 test('divides a / b', () => {
-    // Consider decimals
+    expect(calculator.divide(15,5)).toBe(3);
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(calculator.power(9,2)).toBe(4.5);
 })
 
