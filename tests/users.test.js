14e13095b2dfb2c1d9c6aaa8e95e5b3d32cb79ae
NatLop <nplopez_s@hotmail.com>
2023-03-02 09:56:26 -0600

    actualizacion

--- tests/users.test.js
+++ tests/users.test.js
@@ -5,5 +5,7 @@ const {getUser, getUsers} = require('../users');
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
-
+test ('Usario por id', () => {
+    expect(getUser(1).username).toBe("hpLover4");
+});
 // READ documentation
